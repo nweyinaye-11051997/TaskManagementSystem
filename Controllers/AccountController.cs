4d4c4eca61df97b1bb4b15d2e43de029623676c1
SMYEE <darkpink2016@gmail.com>
2024-12-10 14:03:03 +0630

    refactor code

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -36,38 +37,7 @@ namespace TaskManagementSystem.Controllers
                var rs = await _siginManager.PasswordSignInAsync(view.Username, view.Password, view.RememberMe, false);
                 if (rs.Succeeded)
                 {
-
-                    HttpContext.Session.SetString("Username", view.Username);
-
-                    CookieOptions options = new CookieOptions();
-                    options.Expires = DateTime.Now.AddMinutes(1);
-                    HttpContext.Response.Cookies.Append("Username", view.Username, options);
-
-                    var issuer = _configuration["Jwt:Issuer"];
-                    var audience = _configuration["Jwt:Audience"];
-                    var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
-                    var tokenDescriptor = new SecurityTokenDescriptor
-                    {
-                        Subject = new ClaimsIdentity(new[]
-                        {
-                        new Claim("Id", Guid.NewGuid().ToString()),
-                        new Claim("SessionExpired", DateTime.Now.AddMinutes(30).ToString("o")),
-                        new Claim(JwtRegisteredClaimNames.Email, view.Username),
-                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-                    }),
-                        Expires = DateTime.UtcNow.AddMinutes(10),
-                        Issuer = issuer,
-                        Audience = audience,
-                        SigningCredentials = new SigningCredentials
-                        (new SymmetricSecurityKey(key),
-                        SecurityAlgorithms.HmacSha512Signature)
-                    };
-                    var tokenHandler = new JwtSecurityTokenHandler();
-                    var token = tokenHandler.CreateToken(tokenDescriptor);
-                    var jwtToken = tokenHandler.WriteToken(token);
-                    HttpContext.Response.Headers.Append("jwt_token", jwtToken);
-                    HttpContext.Response.Cookies.Append("jwt_token", jwtToken, options);
-                    HttpContext.Session.SetString("jwt_token", jwtToken);
+                    SetCookies(view);
                     return RedirectToAction("List", "Task");
                 }
                 ModelState.AddModelError("", "Invalid username or password!");
@@ -112,12 +82,45 @@ namespace TaskManagementSystem.Controllers
         public async Task<IActionResult> Logout()
         {
             await _siginManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            HttpContext.Session.Clear();
+            HttpContext.Response.Cookies.Delete("Username");
+            HttpContext.Response.Cookies.Delete("jwt_token");
+            return RedirectToAction("Login", "Account");
         }
 
         public void SetCookies(LoginVM view)
         {
-          
+            HttpContext.Session.SetString("Username", view.Username);
+
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddMinutes(1);
+            HttpContext.Response.Cookies.Append("Username", view.Username, options);
+
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                        new Claim("Id", Guid.NewGuid().ToString()),
+                        new Claim("SessionExpired", DateTime.Now.AddMinutes(30).ToString("o")),
+                        new Claim(JwtRegisteredClaimNames.Email, view.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                    }),
+                Expires = DateTime.UtcNow.AddMinutes(10),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials
+                (new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            HttpContext.Response.Headers.Append("jwt_token", jwtToken);
+            HttpContext.Response.Cookies.Append("jwt_token", jwtToken, options);
+            HttpContext.Session.SetString("jwt_token", jwtToken);
         }
     }
 }
